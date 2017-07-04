b7b835bc3e9b084746ce3113760429ae93cb9d20
root <root@61949b3d2f95>
2017-07-04 07:17:34 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_UFgnPjhZbI",
+  "homePage" : "Main",
+  "name" : "Auto_UFgnPjhZbI",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
